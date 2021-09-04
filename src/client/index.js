430415318cf07a105ef577155a2911e3593f2425
AmrMetawea31 <76723688+AmrMetawea31@users.noreply.github.com>
2021-09-04 19:48:31 +0200

    fixing commit

--- src/client/index.js
+++ src/client/index.js
@@ -7,7 +7,13 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
-console.log(checkForName);
-
+(checkForName);
 alert("I EXIST")
-console.log("CHANGE!!");
+
+
+export{
+     handleSubmit,
+     checkForName
+}
+
+ 
